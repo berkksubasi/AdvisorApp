5def46bb38a11f597a635ba51ab7a0bbe68ac43c
berkksubasi <subasiberk214@gmail.com>
2024-04-18 22:21:48 +0300

    fonLaneDATA.ts refactored;

--- src/data/fonLaneDATA.ts
+++ src/data/fonLaneDATA.ts
@@ -7,34 +7,34 @@ interface fonLaneDATA {
 const fonLaneDATA: fonLaneDATA[] = [
   {
     typologyId: 1,
-    title: "Tipoloji 1",
+    title: "1",
     description: "Televizyon yapımcısı, tüccar, müteahhit, kendi işinin sahibi, yönetici, araştırmacı, yaratıcı iş adamı olabilirler. Bu kulvar sahibi her şeyi alıp satabilir. Harflerin kalitesi zihinsel ağırlıktaysa yazar veya avukat olabilirler."
   },
   {
     typologyId: 2,
-    title: "Tipoloji 2",
+    title: "2",
     description: "Psikolog, öğretmen, sekreter, diyetisyen, hemşire, bebek bakıcısı, hasta bakıcı, gemici, kuaför, dansçı, falcı ve medyum olabilirler. Çalıştıkları iş yerlerinde uyum sağlarlar, herkes için barışı sağlarlar. Doğuştan herkesle uyumludurlar."
   },
   {
     typologyId: 3,
-    title: "Tipoloji 3",
+    title: "3",
     description: "İletişim uzmanı, yazar, şair, tiyatro, aktör, müzik sanatçısı, organizatör, çiçekçi, konuşmacı ve tasarım gerektiren her meslekte çalışabilirler."
 
   },
   {
     typologyId: 4,
-    title: "Tipoloji 4",
+    title: "4",
     description: "Teknisyen, tekniker, harita mühendisi, emlakçı, organizatör, tamirci, inşaatçı, çiftçi, memur, sporcu, yazar, öğretmen olabilirler. Kadınları ev hanımı olup ev ve ev işleri ile uğraşmayı severler."
 
   },
   {
     typologyId: 5,
-    title: "Tipoloji 5",
+    title: "5",
     description: "Avukat, dedektif, reklamcı, gazeteci, yayıncı, aktif satış uzmanı, yabancı dil öğretmeni, turist rehberi olabilirler. Seyyah olarak dünyaya gelmişlerdir. Ofiste kapalı ortamda çalışırlarsa sefil hale düşerler."
   },
   {
     typologyId: 6,
-    title: "Tipoloji 6",
+    title: "6",
     description: "Dizi oyuncusu, hemşire, hastane görevlisi, mimari yazar, fotoğrafçı, evlilik danışmanı, eğitim uzmanı, hizmetçi, gardiyan, güvenlik görevlisi, yaşlıların bakımı, gençlerin eğitimi, toplumun bütünsel geliştirilmesi ile ilgilenirler."
   },
   {
@@ -44,27 +44,27 @@ const fonLaneDATA: fonLaneDATA[] = [
   },
   {
     typologyId: 8,
-    title: "Tipoloji 8",
-    description: "Maliyeci, borsacı emlakçı, şirket başkanı, yönetici, gazete müdürü, ticaret erbabı olabilirler. Büyük işler yapmak isterler. Maddi ve mali konularda başarılıdırlar."
+    title: "8",
+    description: "Komutan, muhtar, holding sahibi, maliyeci, borsacı, banker, yönetmen, idareci, müdür, ticaret erbabı, gemi veya demiryolu nakliyecisi, fabrikatör, büyük boyutlarda alım satım işleri ile uğraşan girişimci, emlakçı, şirket başkanı veya danışmanı, gazete müdürü, uluslararası taşımacılık ve dış ticaret uzmanı, hayır işleri organizatörü, iş adamı, ihalecilik, her düzey yönetici olabilirsiniz. Sizin sloganınız büyük iş yapmaktır. Hayatın bütün maddi ve mali konularında başarılı olurlar. Geniş görüşlülüğü elden bırakmadığınız sürece sınırsız büyümeye yatkınsınızdır."
   },
   {
     typologyId: 9,
-    title: "Tipoloji 9",
+    title: "9",
     description: "Sanatçı, öğretmen, hemşire, doktor, cerrah, şifacı, bio enerji uzmanı, yaşam koçu, yazar olabilirler. Duygularını özgürce ifade ederler."
   },
   {
     typologyId: 11,
-    title: "Tipoloji 11",
+    title: "11",
     description: "Psikolog, bakan, ruhani hareketlerin önderi, mucit, yaşam koçu, astrolog olabilirler. Yenilikçi meslekleri tercih ederler. İdeallerini ortaya koymak isterler."
   },
   {
     typologyId: 19,
-    title: "Tipoloji 19",
+    title: "19",
     description: "Cami hocası, memur, belediye görevlisi, hayır işleri organizatörü olabilirler. Göz önünde olmazlar, arka plandaki rutin destekçilerdir."
   },
   {
     typologyId: 22,
-    title: "Tipoloji 22",
+    title: "22",
     description: "Genel Koordinatör, vali, devlet adamı, Nobel ödülüne aday olmuş insanlardır. İhale uzmanı liderlerdir."
   }
 
