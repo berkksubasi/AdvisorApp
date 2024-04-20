088a62995fea163f8ce755d32ce480d90a8f3df0
berkksubasi <subasiberk214@gmail.com>
2024-04-20 22:19:55 +0300

    Affirmations Data Added;

---
+++ src/data/rootChakraAffirmationsData.ts
@@ -0,0 +1,102 @@
+
+const rootChakraAffirmationsData = [
+  {
+    id: 1,
+    chakra: "Kök Çakra",
+    affirmations: [
+      "Sağlıklıyım ve iyi olan her şeyi seçiyorum.",
+      "Ihtiyacım olan her şeye sahibim.",
+      "Güvende hissediyorum.",
+      "Dönüşmeme yardımcı olan her şeye minnet duyuyorum.",
+      "Gerçeği ve adaleti savunuyorum.",
+      "Yaşamayı seviyorum.",
+      "Dünyayı seviyorum.",
+      "Bedenimi seviyorum.",
+      "Geleceğe güvenle bakıyorum.",
+      "Bütün ihtiyaçlarım anında karşılanıyor.",
+      "Hayat beni destekliyor.",
+      "Hayattan keyif alıyorum.",
+      "Yeterliyim.",
+      "Hayatın akışına güveniyorum."
+    ]
+  },
+  {
+    id: 2,
+    chakra: "Sakral Çakra",
+    affirmations: [
+      "Bedenimi seviyorum.",
+      "Yaşamdan zevk alıyorum.",
+      "Cinselliğim kutsaldır.",
+      "Tutkuluyum.",
+      "Keyif alacağım deneyimler için kendime izin veriyorum.",
+      "Aldığım her nefeste memnuniyet hissediyorum.",
+      "Neşeli ve hayat doluyum.",
+      "Bedenim canlı ve sağlıklı.",
+      "Yaşam sevinciyle doluyum.",
+      "Yaratıcılığım arttı.",
+      "Kendimi seviyor ve olduğum gibi kabul ediyorum.",
+      "Hayat beni her an destekliyor."
+    ]
+  },
+  {
+    id: 3,
+    chakra: "Solar Pleksus Çakra",
+    affirmations: [
+      "Her durumda seçme özgürlüğüne sahibim.",
+      "Güçlü ve cesurum.",
+      "Dengedeyim ve uyum içindeyim.",
+      "Kendime ve başkalarına karşı olan önyargılarımı bırakıyorum.",
+      "Geçmişi huzurla serbest bırakmayı seçiyorum.",
+      "Hayatımda her şey yolunda.",
+      "Tüm deneyimlerime teşekkür ediyorum.",
+      "Bolluk ve bereket içinde yaşıyorum.",
+    ]
+  },
+  {
+    id: 4,
+    chakra: "Kalp Çakrası",
+    affirmations: [
+      "Kendimi hayatın getirdiklerine ve yeniliklere açıyorum.",
+      "Hayata güveniyorum.",
+      "Kendimle ve dünyayla barış içindeyim.",
+      "Bütün duygularımı seviyor ve kabul ediyorum.",
+      "Sorumlulukları yeteri kadar üstleniyorum.",
+      "Bağışıklık sistemim çok güçlü.",
+      "İnsanlara karşı anlayışlıyım.",
+    ]
+  },
+  {
+    id: 5,
+    chakra: "Boğaz Çakrası",
+    affirmations: [
+      "Kendimi en iyi şekilde ifade ediyorum.",
+      "Değişime hazır ve istekliyim.",
+      "Hayatla uyum içindeyim.",
+      "Kendimi ifade etmek benim hakkım ve sorumluluğum.",
+      "Kendimle iletişimim harika."
+    ]
+  },
+  {
+    id: 6,
+    chakra: "Göz Çakrası",
+    affirmations: [
+      "Tüm duygularımla varım.",
+      "Farkındalığım her gün daha çok artıyor.",
+      "Bilincim her gün daha çok genişliyor.",
+      "Bakış açım her gün daha çok genişliyor.",
+      "Her gün daha hoşgörülü oluyorum.",
+      "İçimdeki sevgi her gün daha çok artıyor.",
+    ]
+  },
+  {
+    id: 7,
+    chakra: "Tepe Çakrası",
+    affirmations: [
+      "Hepimiz biriz.",
+      "Yaşamaktan zevk alıyorum.",
+      "Her anıma şükürler olsun.",
+    ]
+  },
+];
+
+export default rootChakraAffirmationsData;
