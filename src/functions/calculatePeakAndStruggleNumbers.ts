469a4d91ecdb802a6b5929e117a8c75afd14ecc0
berkksubasi <subasiberk214@gmail.com>
2024-04-20 22:20:57 +0300

    calculate Peak and struggle number function created;

---
+++ src/functions/calculatePeakAndStruggleNumbers.ts
@@ -0,0 +1,55 @@
+"use strict";
+
+const calculatePeakAndStruggleNumbers = (birthdate: Date) => {
+  const parcalar: number[] = birthdate.toLocaleDateString().split("/").map(Number);
+  const day: number = parcalar[0];
+  const month: number = parcalar[1];
+  const year: number = parcalar[2];
+
+  const reduceToOneDigitExceptException = (num: number): number => {
+    if (num === 11 || num === 22) {
+      return num;
+    } else {
+      while (num > 9) {
+        num = Math.floor(num / 10) + (num % 10);
+      }
+      return num;
+    }
+  };
+
+  const peak1: number = reduceToOneDigitExceptException(day + month);
+  const peak2: number = reduceToOneDigitExceptException(day + year);
+  const peak3: number = reduceToOneDigitExceptException(peak1 + peak2);
+  const peak4: number = reduceToOneDigitExceptException(month + year);
+
+  const reduceToOneDigitExceptExceptionForStruggles = (num: number): number => {
+    while (num > 9) {
+      num = Math.floor(num / 10) + (num % 10);
+    }
+    return num;
+  };
+
+  const struggles1: number = Math.abs(month - day);
+  const struggles2: number = Math.abs(day - year);
+  const struggles3: number = reduceToOneDigitExceptException(Math.abs(peak1 - peak2));
+  const struggles4: number = Math.abs(month - year);
+
+  const detailedStruggles = {
+    struggles1: reduceToOneDigitExceptExceptionForStruggles(struggles1),
+    struggles2: reduceToOneDigitExceptExceptionForStruggles(struggles2),
+    struggles3: reduceToOneDigitExceptExceptionForStruggles(struggles3),
+    struggles4: reduceToOneDigitExceptExceptionForStruggles(struggles4),
+  };
+
+  return {
+    peaks: [peak1, peak2, peak3, peak4],
+    struggles: [
+      detailedStruggles.struggles1,
+      detailedStruggles.struggles2,
+      detailedStruggles.struggles3,
+      detailedStruggles.struggles4,
+    ],
+  };
+};
+
+export default calculatePeakAndStruggleNumbers;
