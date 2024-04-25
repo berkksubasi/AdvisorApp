7bae359565ff90a8dc964f5429df28e344b7a4cf
berkksubasi <subasiberk214@gmail.com>
2024-04-25 20:57:08 +0300

    EditProfile added;

---
+++ src/view/settings/EditProfile.tsx
@@ -0,0 +1,244 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Avatar, Input, Switch } from "react-native-elements";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { CustomButton } from "../../components/button";
+import RNPickerSelect from "react-native-picker-select";
+import { useTheme } from "../../providers/ThemeContext";
+
+const EditProfile: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [birthTime, setBirthTime] = useState("");
+  const [birthPlace, setBirthPlace] = useState("");
+  const [zodiacSign, setZodiacSign] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [relationshipStatus, setRelationshipStatus] = useState("");
+  const [lgbtq, setLGBTQ] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [selectedBirthDate, setSelectedBirthDate] = useState<string | null>(
+    null
+  );
+  const [selectedBirthTime, setSelectedBirthTime] = useState<string | null>(
+    null
+  );
+
+  const { darkMode, toggleTheme } = useTheme();
+
+  const selectAvatar = () => {
+    // Burada fotoğraf seçme işlemi gerçekleştirilebilir
+  };
+
+  const updateProfile = () => {
+    // Burada profil güncelleme işlemi gerçekleştirilebilir
+  };
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+
+  const handleConfirmDate = (date: Date) => {
+    setBirthDate(date.toISOString());
+    setSelectedBirthDate(date.toDateString());
+    hideDatePicker();
+  };
+
+  const handleConfirmTime = (time: Date) => {
+    setBirthTime(time.toISOString());
+    setSelectedBirthTime(time.toLocaleTimeString());
+    hideTimePicker();
+  };
+
+  const renderInput = (
+    placeholder: string,
+    value: string,
+    onChangeText: (text: string) => void
+  ) => {
+    return (
+      <Input
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        inputStyle={styles.input}
+        containerStyle={styles.inputContainer}
+      />
+    );
+  };
+
+  useEffect(() => {
+    if (birthDate !== "") {
+      setSelectedBirthDate(new Date(birthDate).toDateString());
+    }
+    if (birthTime !== "") {
+      setSelectedBirthTime(new Date(birthTime).toLocaleTimeString());
+    }
+  }, [birthDate, birthTime]);
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: darkMode ? "black" : "white" },
+      ]}
+    >
+      <View style={styles.avatarContainer}>
+        <TouchableOpacity onPress={selectAvatar}>
+          <Avatar
+            rounded
+            size="large"
+            source={{ uri: avatarUrl }}
+            containerStyle={styles.avatar}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {renderInput("Ad Soyad", name, setName)}
+      {renderInput("E-posta", email, setEmail)}
+
+      <RNPickerSelect
+        placeholder={{ label: "Cinsiyet Seçiniz", value: null }}
+        onValueChange={(value) => setGender(value)}
+        items={[
+          { label: "Kadın", value: "female" },
+          { label: "Erkek", value: "male" },
+        ]}
+        style={{
+          inputIOS: styles.pickerInput,
+          inputAndroid: styles.pickerInput,
+        }}
+      />
+
+      <TouchableOpacity
+        style={styles.datePickerContainer}
+        onPress={showDatePicker}
+      >
+        <Text style={styles.datePickerText}>
+          {selectedBirthDate
+            ? `Seçilen Doğum Tarihi: ${selectedBirthDate}`
+            : "Doğum Tarihi Seç"}
+        </Text>
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirmDate}
+        onCancel={hideDatePicker}
+        cancelTextIOS="İptal"
+        confirmTextIOS="Tamam"
+      />
+
+      <TouchableOpacity
+        style={styles.datePickerContainer}
+        onPress={showTimePicker}
+      >
+        <Text style={styles.datePickerText}>
+          {selectedBirthTime
+            ? `Seçilen Doğum Saati: ${selectedBirthTime}`
+            : "Doğum Saati Seç"}
+        </Text>
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isTimePickerVisible}
+        mode="time"
+        onConfirm={handleConfirmTime}
+        onCancel={hideTimePicker}
+        is24Hour
+        cancelTextIOS="İptal"
+        confirmTextIOS="Tamam"
+      />
+
+      {renderInput("Doğum Yeri", birthPlace, setBirthPlace)}
+      {renderInput("Burcun", zodiacSign, setZodiacSign)}
+      {renderInput("Meslek", occupation, setOccupation)}
+      {renderInput("İlişki Durumu", relationshipStatus, setRelationshipStatus)}
+
+      <View style={styles.lgbtqContainer}>
+        <Text
+          style={[
+            styles.datePickerText,
+            {
+              justifyContent: "space-between",
+              flexDirection: "row",
+              flex: 1,
+              padding: 10,
+            },
+          ]}
+        >
+          LGBTQ
+        </Text>
+        <Switch
+          value={lgbtq}
+          onValueChange={(value) => setLGBTQ(value)}
+          color="#8576FF"
+        />
+      </View>
+      <CustomButton
+        variant="primary"
+        title="Güncelle"
+        onPress={updateProfile}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    padding: 20,
+  },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  avatar: {
+    borderWidth: 1,
+    borderColor: "#8576FF",
+    marginBottom: 20,
+  },
+  lgbtqContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: "100%",
+  },
+  datePickerContainer: {
+    marginBottom: 20,
+    padding: 10,
+  },
+  datePickerText: {
+    fontSize: 18,
+    color: "#C0C0FF",
+  },
+  pickerInput: {
+    color: "#C0C0FF",
+    fontSize: 20,
+    padding: 10,
+    marginBottom: 20,
+  },
+  input: {
+    color: "#000",
+  },
+});
+
+export default EditProfile;
