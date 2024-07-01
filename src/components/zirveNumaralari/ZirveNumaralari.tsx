ea485b9c5206abba62cefc436c4d907c0fd1fa08
Berk Subaşı <berksubasi@Berk-MBP.home>
2024-07-01 17:43:07 +0300

    zirve yaş aralıklar eklendi;

--- src/components/zirveNumaralari/ZirveNumaralari.tsx
+++ src/components/zirveNumaralari/ZirveNumaralari.tsx
@@ -8,18 +8,37 @@ interface ZirveNumaralariProps {
 
 const ZirveNumaralari: React.FC<ZirveNumaralariProps> = ({ matchedPeaks }) => {
   const { darkMode, toggleTheme } = useTheme();
+
+  const zirveYasAraliklari = [
+    {
+      title: "Birinci Zirve Döngüsü",
+      ageRange: "0 - 35 Yaş",
+    },
+    {
+      title: "İkinci Zirve Döngüsü",
+      ageRange: "36 - 44 Yaş",
+    },
+    {
+      title: "Üçüncü Zirve Döngüsü",
+      ageRange: "45 - 53 Yaş",
+    },
+    {
+      title: "Dördüncü Zirve Döngüsü",
+      ageRange: "54+ Yaş",
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <Text style={[styles.title, { color: darkMode ? "white" : "black" }]}>
-        Zirve Numaraları:
+        Zirve Numaraları ve Yaş Aralıkları:
       </Text>
       {matchedPeaks.map((peak, index) => (
         <View key={index}>
           <Text
             style={[styles.title, { color: darkMode ? "white" : "black" }]}
-          >{`${index + 1}. Zirve: ${
-            peak ? peak.peakTitle : "Bilinmeyen"
-          }`}</Text>
+          >{`${index + 1}. Zirve: ${peak ? peak.peakTitle : "Bilinmeyen"
+            }`}</Text>
           {peak && (
             <Text
               style={[
@@ -30,6 +49,9 @@ const ZirveNumaralari: React.FC<ZirveNumaralariProps> = ({ matchedPeaks }) => {
               {peak.peakDescription}
             </Text>
           )}
+          <Text style={[styles.descriptionItem, { color: darkMode ? "white" : "black", fontWeight: "bold" }]}>
+            {zirveYasAraliklari[index].title}: {zirveYasAraliklari[index].ageRange}
+          </Text>
         </View>
       ))}
     </View>
--- src/functions/calculatePeakAndStruggleNumbers.ts
+++ src/functions/calculatePeakAndStruggleNumbers.ts
@@ -1,12 +1,14 @@
 "use strict";
 
 const calculatePeakAndStruggleNumbers = (birthdate: Date) => {
-  const options = { day: 'numeric', month: 'numeric', year: 'numeric' }; // Tarih formatı seçenekleri
-  const parcalar: number[] = birthdate.toLocaleDateString('en-US', options).split("/").map(Number); // İngilizce tarih formatı 
-  const day: number = parcalar[1];
-  const month: number = parcalar[0];
+  // Doğum tarihini gün, ay, yıl olarak parçalama
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  const parcalar: number[] = birthdate.toLocaleDateString('tr-TR', options).split(".").map(Number);
+  const day: number = parcalar[0];
+  const month: number = parcalar[1];
   const year: number = parcalar[2];
 
+  // Tek haneli sayıya indirgeme işlemi (11 ve 22 hariç)
   const reduceToOneDigitExceptException = (num: number): number => {
     if (num === 11 || num === 22) {
       return num;
@@ -18,11 +20,19 @@ const calculatePeakAndStruggleNumbers = (birthdate: Date) => {
     }
   };
 
+  // Zirve hesaplamaları
   const peak1: number = reduceToOneDigitExceptException(day + month);
   const peak2: number = reduceToOneDigitExceptException(day + year);
   const peak3: number = reduceToOneDigitExceptException(peak1 + peak2);
   const peak4: number = reduceToOneDigitExceptException(month + year);
 
+  // Mücadele hesaplamaları
+  const struggles1: number = Math.abs(month - day);
+  const struggles2: number = Math.abs(day - year);
+  const struggles3: number = reduceToOneDigitExceptException(Math.abs(peak1 - peak2));
+  const struggles4: number = Math.abs(month - year);
+
+  // Tek haneli sayıya indirgeme işlemi (Mücadele için)
   const reduceToOneDigitExceptExceptionForStruggles = (num: number): number => {
     while (num > 9) {
       num = Math.floor(num / 10) + (num % 10);
@@ -30,11 +40,7 @@ const calculatePeakAndStruggleNumbers = (birthdate: Date) => {
     return num;
   };
 
-  const struggles1: number = Math.abs(month - day);
-  const struggles2: number = Math.abs(day - year);
-  const struggles3: number = reduceToOneDigitExceptException(Math.abs(peak1 - peak2));
-  const struggles4: number = Math.abs(month - year);
-
+  // Detaylı Mücadele hesaplamaları
   const detailedStruggles = {
     struggles1: reduceToOneDigitExceptExceptionForStruggles(struggles1),
     struggles2: reduceToOneDigitExceptExceptionForStruggles(struggles2),
@@ -42,6 +48,7 @@ const calculatePeakAndStruggleNumbers = (birthdate: Date) => {
     struggles4: reduceToOneDigitExceptExceptionForStruggles(struggles4),
   };
 
+  // Sonuçları döndürme
   return {
     peaks: [peak1, peak2, peak3, peak4],
     struggles: [
